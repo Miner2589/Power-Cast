acfa2c397fa60fd1dff2d43206f92e52d100b3e2
Miner2589 <154419396+Miner2589@users.noreply.github.com>
2026-02-04 16:35:44 +0000

    v1.0 source code

---
+++ Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace Power_Cast
+{
+    public partial class Form1 : Form
+    {
+        private Socket httpServer;
+        private int serverPort = 80;
+        private Thread thread;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void startServerBtn_Click(object sender, EventArgs e)
+        {
+            serverLogsText.Text = "";
+
+            try 
+            {
+                httpServer = new Socket(SocketType.Stream, ProtocolType.Tcp);
+
+                try
+                {
+                    serverPort = int.Parse(serverPortText.Text.ToString());
+
+                    if (serverPort > 65535 || serverPort <= 0)
+                    {
+                        new Exception("Server Port not within range");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    serverPort = 80;
+                    serverLogsText.Text = "Server Failed to start on specified port \n";
+                }
+
+                thread = new Thread(new ThreadStart(this.connectionThreadMethod));
+                thread.Start();
+
+                //disable and enable buttons
+                startServerBtn.Enabled = false;
+                stopServerBtn.Enabled = true;
+
+            }
+            catch(Exception ex) 
+            {
+                Console.WriteLine("Error While Starting server \n");
+                serverLogsText.Text = "PCS Failed to start\n";
+            }
+
+            serverLogsText.Text = "Power Cast Server Started\n";
+        }
+
+        private void stopServerBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Close the Socket
+                httpServer.Close();
+
+                // Kill the Thread
+                thread.Abort();
+
+                // Disable and Enable Buttons
+                startServerBtn.Enabled = true;
+                stopServerBtn.Enabled = false;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Stopping PCS Failed\n");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            stopServerBtn.Enabled = false;
+        }
+
+        private void connectionThreadMethod()
+        {
+            try
+            {
+                IPEndPoint endpoint = new IPEndPoint(IPAddress.Any, serverPort);
+                httpServer.Bind(endpoint);
+                httpServer.Listen(1);
+                startListeningForConnection();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("i could not start");
+            }
+        }
+
+        private void startListeningForConnection() 
+        {
+            while(true)
+            {
+                DateTime time = DateTime.Now;
+
+                string data = "";
+                byte[] bytes = new byte[2048];
+
+                Socket client = httpServer.Accept(); // blocking statement
+
+                // reading the inbound connection data
+                while (true)
+                {
+                    int numBytes = client.Receive(bytes);
+                    data += Encoding.ASCII.GetString(bytes, 0, numBytes);
+
+                    if (data.IndexOf("\r\n") > -1)
+                        break;
+                }
+
+                //data read
+
+                serverLogsText.Invoke((MethodInvoker)delegate
+                {
+                    // runs inside UI thread
+                    serverLogsText.Text += "\r\n\r\n";
+                    serverLogsText.Text += data;
+                    serverLogsText.Text += "\n\n-------- End Of Request --------";
+
+                });
+
+                //send back responce
+                String resHeader = "HTTP/1.1 200 Everything is Fine\nServer: Power_Cast_V1.0\nContent-Type: text/html; charset: UTF-8\n\n";
+                String resBody = html.Text;
+
+                String resStr = resHeader + resBody;
+
+                byte[] resData = Encoding.ASCII.GetBytes(resStr);
+
+                client.SendTo(resData, client.RemoteEndPoint);
+
+                client.Close();
+            }
+        }
+    }
+}
---
+++ Form2.Designer.cs
@@ -0,0 +1,176 @@
+﻿
+namespace Power_Cast
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 97);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(268, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Power Cast Server Version 1.1.2";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(17, 121);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(232, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Created By Oliver Harry Stuchbery";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(17, 139);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(258, 17);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "This Version Published: 11th April 2025";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(18, 157);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(276, 17);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Published By Stuchbery Computer Group";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(18, 175);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(227, 17);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Help And Support By SRNetworks";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(18, 192);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(125, 18);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "HTML version 1.1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Image = global::Power_Cast.Properties.Resources.power_cast_banner_blank_version;
+            this.pictureBox1.Location = new System.Drawing.Point(-1, -1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(429, 91);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Arial Black", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(287, 57);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(98, 33);
+            this.label7.TabIndex = 21;
+            this.label7.Text = "V1.1.2";
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox6.Location = new System.Drawing.Point(-1, 88);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(429, 288);
+            this.pictureBox6.TabIndex = 23;
+            this.pictureBox6.TabStop = false;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(427, 374);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form2";
+            this.Text = "About";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.PictureBox pictureBox6;
+    }
+}
